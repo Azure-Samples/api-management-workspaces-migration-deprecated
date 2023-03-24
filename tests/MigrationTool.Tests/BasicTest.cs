16371c1562c250c76e3374409ef901c60ce1a4ec
Tom Kerkhove <kerkhove.tom@gmail.com>
2023-03-24 14:10:06 +0100

    feat: Provide basic CI (#18
    * feat: Provide basic CI
    
    * Fetch submodules
    
    * Fix submodule after moving
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Add foundation for unit tests
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Fix test filter path
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Make it work for forks
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    ---------
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ tests/MigrationTool.Tests/BasicTest.cs
@@ -0,0 +1,20 @@
+// --------------------------------------------------------------------------
+//  <copyright file="BasicTest.cs" company="Microsoft">
+//      Copyright (c) Microsoft Corporation. All rights reserved.
+//  </copyright>
+// --------------------------------------------------------------------------
+
+namespace MigrationTool.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BasicTest
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
